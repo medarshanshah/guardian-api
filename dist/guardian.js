8a8984f6673df8a4f8b78dbd7a44a2ed8a954c95
Darshan <darshanshah.ds2017@gmail.com>
2023-10-05 23:48:40 +0545

    optimized code, added section

--- dist/guardian.js
+++ dist/guardian.js
@@ -3,15 +3,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.homepage = exports.guardianRoute = void 0;
+exports.callGuardianAPI = exports.guardianRoute = void 0;
 const axios_1 = __importDefault(require("axios"));
 const express_1 = require("express");
 exports.guardianRoute = (0, express_1.Router)();
-const homepage = () => {
+const callGuardianAPI = (route_origin, api_url) => {
     try {
-        return axios_1.default.get('https://content.guardianapis.com/search?api-key=test&section=(world|sport|business|technology|politics)')
+        return axios_1.default.get(api_url)
             .then(response => {
-            console.log("homepage: status:: ", response.status);
+            console.log(route_origin, ": status:: ", response.status);
             return response.data.response.results;
         });
     }
@@ -19,9 +19,38 @@ const homepage = () => {
         console.log(error);
     }
 };
-exports.homepage = homepage;
+exports.callGuardianAPI = callGuardianAPI;
+// export const sections = () => {
+//     try {
+//         return axios.get('https://content.guardianapis.com/sections?api-key=test')
+//         .then(response => {
+//             console.log("sections: status::",response.status)
+//             return response.data.response.results;
+//         })
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+//Routes
+//home
 exports.guardianRoute.get('/', (req, res) => {
-    (0, exports.homepage)().then(data => {
+    const home_url = 'https://content.guardianapis.com/search?api-key=test&section=(world|sport|business|technology|politics)';
+    (0, exports.callGuardianAPI)('homepage', home_url).then(data => {
+        res.json(data);
+    });
+});
+//section
+exports.guardianRoute.get('/section', (req, res) => {
+    const section_url = 'https://content.guardianapis.com/sections?api-key=test';
+    (0, exports.callGuardianAPI)('section', section_url).then(data => {
+        res.json(data);
+    });
+});
+exports.guardianRoute.get('/section/:sectionId', (req, res) => {
+    console.log(req.params.sectionId);
+    const sectionId = req.params.sectionId;
+    const section_url = `https://content.guardianapis.com/${sectionId}?api-key=test`;
+    (0, exports.callGuardianAPI)('section', section_url).then(data => {
         res.json(data);
     });
 });
--- src/guardian.ts
+++ src/guardian.ts
@@ -4,33 +4,56 @@ import { Router } from 'express';
 
 export const guardianRoute = Router();
 
-export const homepage = () => {
+export const callGuardianAPI = (route_origin:string, api_url:string) => {
     try {
-        return axios.get('https://content.guardianapis.com/search?api-key=test&section=(world|sport|business|technology|politics)')
+        return axios.get(api_url)
         .then(response => {
-            console.log("homepage: status:: ",response.status);
-            return response.data.response.results
+            console.log(route_origin,": status:: ",response.status);
+            return response.data.response.results;
         })
     } catch (error) {
         console.log(error);
     }
-    
-        
 }
+
+// export const sections = () => {
+//     try {
+//         return axios.get('https://content.guardianapis.com/sections?api-key=test')
+//         .then(response => {
+//             console.log("sections: status::",response.status)
+//             return response.data.response.results;
+//         })
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
 
 
 //Routes
 
 //home
 guardianRoute.get('/', (req, res) => {
-    homepage().then(data => {
+    const home_url = 'https://content.guardianapis.com/search?api-key=test&section=(world|sport|business|technology|politics)';
+    callGuardianAPI('homepage', home_url).then(data => {
         res.json(data);
     })
 })
 
 //section
-guardianRoute.get('/:sectionId', (req, res) => {
+guardianRoute.get('/section', (req, res) => {
+    const section_url = 'https://content.guardianapis.com/sections?api-key=test';
+    callGuardianAPI('section', section_url).then(data => {
+        res.json(data);
+    })
+})
 
+guardianRoute.get('/section/:sectionId', (req, res) => {
+    console.log(req.params.sectionId)
+    const sectionId = req.params.sectionId;
+    const section_url = `https://content.guardianapis.com/${sectionId}?api-key=test`;
+    callGuardianAPI('section', section_url).then(data => {
+        res.json(data);
+    })
 })
 
 module.exports = {
