6b0e40290cf25152813a5c083827c55088718301
Darshan <darshanshah.ds2017@gmail.com>
2023-10-05 12:13:41 +0545

    successful test of guardian api

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,26 @@
 import express from 'express';
+import axios from 'axios';
 
 const app = express();
+const cors = require('cors');
+app.use(cors());
+
 const port = 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+app.get('/test', async (req, res) => {
+    const data:object = await axios.get('https://content.guardianapis.com/search?q=debates&api-key=test')
+    .then(response => {
+        console.log("status ---->  ", response.status)
+        console.log("response ---->  ",response)
+        return response.data;
+    })
+    res.send(data);
+
+})
 
 app.listen(port, () => {
     console.log('listening to port: ',port);
